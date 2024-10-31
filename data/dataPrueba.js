5dc315218e742db6e44ba05f6c0906a0fc2a5401
Val-jimenez <vjimeneza@uninorte.edu.co>
2024-10-31 09:21:24 -0500

    First commit API

---
+++ data/dataPrueba.js
@@ -0,0 +1,11 @@
+const Prueba = [
+    {
+        id: 1,
+        nombre: 'Prueba 1',
+        descripcion: 'Descripcion de la prueba 1',
+        precio: 10000,
+        imagen: 'https://via.placeholder.com/150'
+    },
+]
+
+module.exports = Prueba;
---
+++ db.js
@@ -0,0 +1,9 @@
+const Prueba = require('./data/dataPrueba');
+
+
+module.exports = () => {
+  return {
+    Prueba
+    //Exportar datos de la base de datos
+};
+}
