b48b4864fa54186a62db672b23a47bcbe1e2a575
Val-jimenez <vjimeneza@uninorte.edu.co>
2024-11-03 15:07:58 -0500

    Dummy clients were added for trials

---
+++ data/clients.js
@@ -0,0 +1,70 @@
+const Clients = [
+    {
+        "id": "1",
+        "nit": "1",
+        "name": "Valeria Jimenez",
+        "address": "Carrera 38 C#74B 31 SEGUNDO PISO",
+        "city": "BARRANQUILLA (ATL)",
+        "country": "Colombia",
+        "phone": "3002833359",
+        "email": "valeriajimenez0406@gmail.com",
+        "active": true
+      },
+      
+        {
+          "id": "2",
+          "nit": "2",
+          "name": "Carlos Rodríguez",
+          "address": "Calle 54 #45-22 OFICINA 201",
+          "city": "BARRANQUILLA (ATL)",
+          "country": "Colombia",
+          "phone": "3104567890",
+          "email": "carlosrodriguez@gmail.com",
+          "active": true
+        },
+        {
+          "id": "3",
+          "nit": "3",
+          "name": "Andrea López",
+          "address": "Carrera 43 #79B-56",
+          "city": "BARRANQUILLA (ATL)",
+          "country": "Colombia",
+          "phone": "3009876543",
+          "email": "andrealopz@hotmail.com",
+          "active": true
+        },
+        {
+          "id": "4",
+          "nit": "4",
+          "name": "Miguel Torres",
+          "address": "Carrera 46 #85-12 APARTAMENTO 304",
+          "city": "BARRANQUILLA (ATL)",
+          "country": "Colombia",
+          "phone": "3123456789",
+          "email": "migueltorres@hotmail.com",
+          "active": true
+        },
+        {
+          "id": "5",
+          "nit": "5",
+          "name": "Laura Martínez",
+          "address": "Calle 75 #41-30 EDIFICIO MIRADOR",
+          "city": "BARRANQUILLA (ATL)",
+          "country": "Colombia",
+          "phone": "3012345678",
+          "email": "lauramartinez@gmail.com",
+          "active": true
+        },
+        {
+          "id": "6",
+          "nit": "6",
+          "name": "Ricardo Gómez",
+          "address": "Carrera 52 #80-15 LOCAL 10",
+          "city": "BARRANQUILLA (ATL)",
+          "country": "Colombia",
+          "phone": "3156789102",
+          "email": "ricardogomez@gmail.com",
+          "active": true
+        }
+      ]
+      
--- db.js
+++ db.js
@@ -1,10 +1,8 @@
-const Prueba = require('./data/dataPrueba');
 const clients =require("./data/clients");
 
 
 module.exports = () => {
   return {
-    Prueba,
     clients
     //Exportar datos de la base de datos
 };
